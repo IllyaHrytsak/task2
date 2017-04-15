c80f963ef14cf1090ef135ed4ccf4b1f060bf633
Illya Hrytsak <gritsak_web@bigmir.net>
2017-04-15 18:16:39 +0300

    Added small changes to view

--- src/main/java/ua/training/View.java
+++ src/main/java/ua/training/View.java
@@ -1,33 +1,47 @@
 package ua.training;
 
-
-import java.util.ArrayList;
+import java.util.List;
 
 public class View {
 
     public static final String INPUT_NUMBER = "Input number: ";
     public static final String WRONG_NUMBER = "Wrong number: ";
     public static final String WRONG_INPUT_DATA = "Wrong input data";
-    public static final String ALL_STATISTIC = "Statistic: ";
-    public static final String NUMBER_OF_ATTEMPTS = "Number of attempts: ";
-    public static final String YOU_WINNER = "You guessed the number!";
+    public static final String NUMBER_OF_ATTEMPTS = "Attempts: ";
+    public static final String YOU_WINNER = "You guessed the number! ";
+    public static final String Range_IS = "Range: ";
+    public static final String TO = " - ";
+    public static final String SPACE = " ";
+    public static final String ENTERED_NUMBERS = "Entered numbers: ";
 
-    public void showStatistic(ArrayList<Integer> arrayList) {
-        System.out.print(ALL_STATISTIC);
+    public void showStatistic(Model model) {
+        List<Integer> arrayList = model.getStatistic();
+        System.out.println(NUMBER_OF_ATTEMPTS + arrayList.size());
+        System.out.print(ENTERED_NUMBERS);
         for (int digit : arrayList) {
-            System.out.printf("[%d]", digit);
+            System.out.print(digit+SPACE);
         }
         System.out.println();
-        System.out.println(NUMBER_OF_ATTEMPTS + arrayList.size());
     }
 
-    public void showRange(int min, int max) {
-
-        System.out.printf("Range: %d - %d\n", min, max);
+    public void showRange(Model model) {
+        System.out.println(Range_IS + model.getMinRange() + TO + model.getMaxRange());
     }
 
-    public void finalResult(ArrayList<Integer> arrayList) {
+    public void showWrongInputData() {
+        System.out.println(WRONG_INPUT_DATA + INPUT_NUMBER);
+    }
+
+    public void showWrongInputNumber(int number) {
+        System.out.println(WRONG_NUMBER + number);
+    }
+
+    public void inputData() {
+        System.out.print(INPUT_NUMBER);
+    }
+
+    public void finalResult(Model model) {
         System.out.println(YOU_WINNER);
-        showStatistic(arrayList);
+        showStatistic(model);
     }
 }
