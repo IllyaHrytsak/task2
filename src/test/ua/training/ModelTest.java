b7a826bb4a5c64a877747c77231adc3f5dd4b601
Illya Hrytsak <gritsak_web@bigmir.net>
2017-04-15 18:18:46 +0300

    Added new testes and removed unnecessary

--- src/test/ua/training/ModelTest.java
+++ src/test/ua/training/ModelTest.java
@@ -5,45 +5,48 @@ import org.junit.Test;
 
 
 public class ModelTest {
-    private static final Model model = new Model();
-
 
     @Test
     public void testFixedRand() throws Exception {
-        int min = 0;
-        int max = 100;
+        Model model = new Model();
         for (int i = 0; i < 100; i++) {
-            int randNumber = model.rand();
-            Assert.assertTrue(randNumber >= min && randNumber <= max);
+            model.rand();
+            int randNumber = model.getSecretNumber();
+            Assert.assertTrue(randNumber >= GlobalValues.PRIMARY_MIN_BARRIER
+                                    && randNumber <= GlobalValues.PRIMARY_MAX_BARRIER);
         }
     }
 
     @Test
-    public void testFlexibleRand() throws Exception {
+    public void testFlexibleRandForFiveAndFifteen() throws Exception {
+        Model model = new Model();
         int min = 5;
         int max = 15;
+        model.setMinAndMaxRange(min, max);
         for (int i = 0; i < 100; i++) {
-            int randNumber = model.rand(min, max);
+            model.rand();
+            int randNumber = model.getSecretNumber();
             Assert.assertTrue(randNumber >= min && randNumber <= max);
         }
     }
 
     @Test
-    public void testAddNumber() throws Exception {
+    public void testFlexibleRandForTenAndSixty() throws Exception {
+        Model model = new Model();
+        int min = 10;
+        int max = 60;
+        model.setMinAndMaxRange(min, max);
         for (int i = 0; i < 100; i++) {
-            int randNumber = model.rand();
-            model.addNumber(randNumber);
+            model.rand();
+            int randNumber = model.getSecretNumber();
+            Assert.assertTrue(randNumber >= min && randNumber <= max);
         }
-        Assert.assertEquals(100, model.getStatistic().size());
     }
 
-    @Test
-    public void testGetStatistic() throws Exception {
-        Assert.assertNotNull(model.getStatistic());
-    }
 
     @Test
     public void testCheckEquality() throws Exception {
+        Model model = new Model();
         int number1 = 10;
         int number2 = 20;
         boolean result1 = model.checkNotEqual(number1, number2);
